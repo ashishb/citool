427b8ff3f4e369fe20456b1887f86865a3cc20e7
Ashish Bhatia <ashishb@ashishb.net>
2019-07-11 10:08:29 -0700

    More improvements including success status

--- citool.go
+++ citool.go
@@ -17,7 +17,10 @@ type CircleCiBuildWorkflow struct {
 type CircleCiBuildResult struct {
 	Username    string                `json:"username"`
 	Reponame    string                `json:"reponame"`
+	Branch		string				  `json:"branch"`
 	BuildNumber int                   `json:"build_num"`
+	// :retried, :canceled, :infrastructure_fail, :timedout, :not_run, :running,
+	// :failed, :queued, :scheduled, :not_running, :no_tests, :fixed, :success
 	Status      string                `json:"status"`
 	EndTime     string                `json:"stop_time"`
 	StartTime   string                `json:"start_time"`
@@ -38,72 +41,109 @@ func getJson(filename string) []CircleCiBuildResult {
 }
 
 func main() {
-	data1 := getJson("./3.json")
-	data2 := getJson("./4.json")
-	data := append(data1, data2...)
+	i := 1
+	data := make([]CircleCiBuildResult, 0)
+	for i < 11 {
+		tmp := getJson(fmt.Sprintf("./%d.json", i))
+		data = append(data, tmp...)
+		i += 1
+	}
 	printTestStats(data)
 }
 
 // Aggregated test information
 type AggregateTestInfo struct {
+	TestName	string
 	Frequency          int
 	CumulativeDuration time.Duration
+	SuccessCount int32
+	FailureCount int32
 }
-
-type TestNameAndDurationPair struct {
-	TestName string
-	Duration time.Duration
-}
-
-type TestNameAndDurationPairList []TestNameAndDurationPair
 
 func printTestStats(results []CircleCiBuildResult) {
+	fmt.Printf("Number of test results: %d\n", len(results))
 	aggregateTestInfo := make(map[string]*AggregateTestInfo, 0)
 	for _, result := range results {
 		testName := result.Workflows.JobName
 		status := result.Status
-		// Only filter on success for now
-		if status != "success" {
+		// Only filter on success/failure for now
+		if status != "success" && status != "failed" {
 			continue
 		}
 		duration := getTestDuration(result)
 		existingAggregateTestInfo, present := aggregateTestInfo[testName]
 		if !present {
-			existingAggregateTestInfo = &AggregateTestInfo{0, time.Duration(0)}
+			existingAggregateTestInfo = &AggregateTestInfo{testName, 0, time.Duration(0), 0, 0}
 		}
 		existingAggregateTestInfo.Frequency += 1
 		existingAggregateTestInfo.CumulativeDuration += duration
 		aggregateTestInfo[testName] = existingAggregateTestInfo
 		aggregateTestInfo[testName].Frequency = aggregateTestInfo[testName].Frequency + 1
+		if status == "success" {
+			existingAggregateTestInfo.SuccessCount += 1
+		} else if status == "failure" {
+			existingAggregateTestInfo.FailureCount += 1
+		}
 	}
 
-	testNameAndDurationList := make(TestNameAndDurationPairList, 0)
-	for testName, testInfo := range aggregateTestInfo {
-		averageDuration := time.Duration(testInfo.CumulativeDuration.Nanoseconds() / int64(testInfo.Frequency))
-		testNameAndDurationList = append(
-			testNameAndDurationList, TestNameAndDurationPair{testName, averageDuration})
+	values := make([]*AggregateTestInfo, 0, len(aggregateTestInfo))
+	for _, v := range aggregateTestInfo {
+		values = append(values, v)
 	}
-	sort.Slice(testNameAndDurationList, func(i, j int) bool {
-		return testNameAndDurationList[i].Duration > testNameAndDurationList[j].Duration
-	})
 
-    printTestDuration(testNameAndDurationList)
+	printTestDuration(values)
+	fmt.Println("")
+	printTestSuccessRate(values)
 }
 
-func printTestDuration(testNameAndDurationList TestNameAndDurationPairList) {
-    writer := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
-    //noinspection GoUnhandledErrorResult
-    fmt.Fprintln(writer, "Test name\tAverage test duration")
-    //noinspection GoUnhandledErrorResult
-    fmt.Fprintln(writer, "----------\t--------------------")
-    for _, testNameAndDuration := range testNameAndDurationList {
-        // We don't need accuracy below one second.
-        averageDuration := testNameAndDuration.Duration.Round(time.Second)
-        //noinspection GoUnhandledErrorResult
-        fmt.Fprintln(writer, fmt.Sprintf("%s\t%v", testNameAndDuration.TestName, averageDuration))
-    }
-    //noinspection GoUnhandledErrorResult
-    writer.Flush()
+func printTestDuration(aggregateTestInfo []*AggregateTestInfo) {
+	sort.Slice(aggregateTestInfo, func(i, j int) bool {
+		averageDuration1 := time.Duration(aggregateTestInfo[i].CumulativeDuration.Nanoseconds() / int64(aggregateTestInfo[i].Frequency))
+		averageDuration2 := time.Duration(aggregateTestInfo[j].CumulativeDuration.Nanoseconds() / int64(aggregateTestInfo[j].Frequency))
+		return averageDuration1 > averageDuration2
+	})
+	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+	//noinspection GoUnhandledErrorResult
+	fmt.Fprintln(writer, "Test name\tAverage test duration")
+	//noinspection GoUnhandledErrorResult
+	fmt.Fprintln(writer, "----------\t--------------------")
+	for _, v := range aggregateTestInfo {
+		averageDuration := time.Duration(v.CumulativeDuration.Nanoseconds() / int64(v.Frequency))
+		// We don't need accuracy below one second.
+		averageDuration = averageDuration.Round(time.Second)
+		//noinspection GoUnhandledErrorResult
+		fmt.Fprintln(writer, fmt.Sprintf("%s\t%v", v.TestName, averageDuration))
+	}
+	//noinspection GoUnhandledErrorResult
+	writer.Flush()
+}
+
+func printTestSuccessRate(aggregateTestInfo []*AggregateTestInfo) {
+	for _, v := range aggregateTestInfo {
+		aggregateTestInfo = append(aggregateTestInfo, v)
+	}
+	sort.Slice(aggregateTestInfo, func(i, j int) bool {
+		return (
+			aggregateTestInfo[i].SuccessCount * aggregateTestInfo[j].FailureCount >
+				aggregateTestInfo[j].SuccessCount * aggregateTestInfo[i].FailureCount)
+	})
+
+	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+	//noinspection GoUnhandledErrorResult
+	fmt.Fprintln(writer, "Test name\tSuccess Rate")
+	//noinspection GoUnhandledErrorResult
+	fmt.Fprintln(writer, "----------\t-----------")
+	for _, v := range aggregateTestInfo {
+		successRate := int32(0)
+		if v.SuccessCount + v.FailureCount > 0 {
+			successRate = (100 * v.SuccessCount) / (v.SuccessCount + v.FailureCount)
+		}
+		//noinspection GoUnhandledErrorResult
+		fmt.Fprintln(writer, fmt.Sprintf("%s\t%d/%d (%d%%)",
+			v.TestName, v.SuccessCount, v.SuccessCount + v.FailureCount, successRate))
+	}
+	//noinspection GoUnhandledErrorResult
+	writer.Flush()
 }
 
 func getTime(timeString string) time.Time {
