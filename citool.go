a91418b2494db17502bea1e33312c8f61d517d53
Ashish Bhatia <ashishb@ashishb.net>
2019-07-11 11:08:35 -0700

    Add branch, testname, and test result based filtering

--- citool.go
+++ citool.go
@@ -1,161 +1,97 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"sort"
-	"text/tabwriter"
-	"time"
+	"robpike.io/filter"
+	"strings"
 )
 
-type CircleCiBuildWorkflow struct {
-	JobName string `json:"job_name"`
-}
+var branchFilter = flag.String("only-branch",
+	"",
+	"Only consider test results from this branch")
 
-type CircleCiBuildResult struct {
-	Username    string                `json:"username"`
-	Reponame    string                `json:"reponame"`
-	Branch		string				  `json:"branch"`
-	BuildNumber int                   `json:"build_num"`
-	// :retried, :canceled, :infrastructure_fail, :timedout, :not_run, :running,
-	// :failed, :queued, :scheduled, :not_running, :no_tests, :fixed, :success
-	Status      string                `json:"status"`
-	EndTime     string                `json:"stop_time"`
-	StartTime   string                `json:"start_time"`
-	Workflows   CircleCiBuildWorkflow `json:"workflows"`
-}
+var testnameFilter = flag.String("only-testname",
+	"",
+	"Only consider test results for this testname")
 
-func getJson(filename string) []CircleCiBuildResult {
-	contents, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic("Unable to read file " + filename)
-	}
-	var circleCiBuildResults []CircleCiBuildResult
-	err2 := json.Unmarshal(contents, &circleCiBuildResults)
-	if err2 != nil {
-		panic("Failed to extract JSON" + err2.Error())
-	}
-	return circleCiBuildResults
-}
+var testStatusFilter = flag.String("only-testresult",
+	"",
+	"Only consider test results with this completion status")
+
+var inputFiles = flag.String("input-files",
+	"",
+	"Comma-separated list of files containing downloaded test results from CircleCI")
+
+const debug = true
 
 func main() {
-	i := 1
+	flag.Parse()
+
+	files := strings.Split(*inputFiles, ",")
+	testResults := getCircleCiBuildResults(&files)
+	filterData(&testResults)
+	PrintTestStats(testResults)
+}
+
+func getCircleCiBuildResults(files *[]string) []CircleCiBuildResult {
 	data := make([]CircleCiBuildResult, 0)
-	for i < 11 {
-		tmp := getJson(fmt.Sprintf("./%d.json", i))
-		data = append(data, tmp...)
-		i += 1
-	}
-	printTestStats(data)
-}
-
-// Aggregated test information
-type AggregateTestInfo struct {
-	TestName	string
-	Frequency          int
-	CumulativeDuration time.Duration
-	SuccessCount int32
-	FailureCount int32
-}
-
-func printTestStats(results []CircleCiBuildResult) {
-	fmt.Printf("Number of test results: %d\n", len(results))
-	aggregateTestInfo := make(map[string]*AggregateTestInfo, 0)
-	for _, result := range results {
-		testName := result.Workflows.JobName
-		status := result.Status
-		// Only filter on success/failure for now
-		if status != "success" && status != "failed" {
+	for _, file := range *files {
+		logDebug("Input file: " + file)
+		// Ignore empty file names
+		if len(file) == 0 {
 			continue
 		}
-		duration := getTestDuration(result)
-		existingAggregateTestInfo, present := aggregateTestInfo[testName]
-		if !present {
-			existingAggregateTestInfo = &AggregateTestInfo{testName, 0, time.Duration(0), 0, 0}
+		tmp := GetJson(file)
+		data = append(data, tmp...)
+	}
+	return data
+}
+
+func filterData(results *[]CircleCiBuildResult) {
+	if len(*branchFilter) > 0 {
+		logDebug("Filtering on branch: " + *branchFilter)
+	}
+	if len(*testnameFilter) > 0 {
+		logDebug("Filtering on test name: " + *testnameFilter)
+	}
+	if len(*testStatusFilter) > 0 {
+		logDebug("Filtering on test result: " + *testStatusFilter)
+	}
+	filter.ChooseInPlace(results, filterRule)
+}
+
+func filterRule(result CircleCiBuildResult) bool {
+	if len(*branchFilter) > 0 {
+		if result.Branch != *branchFilter {
+			return false
 		}
-		existingAggregateTestInfo.Frequency += 1
-		existingAggregateTestInfo.CumulativeDuration += duration
-		aggregateTestInfo[testName] = existingAggregateTestInfo
-		aggregateTestInfo[testName].Frequency = aggregateTestInfo[testName].Frequency + 1
-		if status == "success" {
-			existingAggregateTestInfo.SuccessCount += 1
-		} else if status == "failure" {
-			existingAggregateTestInfo.FailureCount += 1
+	}
+	if len(*testnameFilter) > 0 {
+		if result.Workflows.JobName != *testnameFilter {
+			return false
 		}
 	}
-
-	values := make([]*AggregateTestInfo, 0, len(aggregateTestInfo))
-	for _, v := range aggregateTestInfo {
-		values = append(values, v)
-	}
-
-	printTestDuration(values)
-	fmt.Println("")
-	printTestSuccessRate(values)
-}
-
-func printTestDuration(aggregateTestInfo []*AggregateTestInfo) {
-	sort.Slice(aggregateTestInfo, func(i, j int) bool {
-		averageDuration1 := time.Duration(aggregateTestInfo[i].CumulativeDuration.Nanoseconds() / int64(aggregateTestInfo[i].Frequency))
-		averageDuration2 := time.Duration(aggregateTestInfo[j].CumulativeDuration.Nanoseconds() / int64(aggregateTestInfo[j].Frequency))
-		return averageDuration1 > averageDuration2
-	})
-	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
-	//noinspection GoUnhandledErrorResult
-	fmt.Fprintln(writer, "Test name\tAverage test duration")
-	//noinspection GoUnhandledErrorResult
-	fmt.Fprintln(writer, "----------\t--------------------")
-	for _, v := range aggregateTestInfo {
-		averageDuration := time.Duration(v.CumulativeDuration.Nanoseconds() / int64(v.Frequency))
-		// We don't need accuracy below one second.
-		averageDuration = averageDuration.Round(time.Second)
-		//noinspection GoUnhandledErrorResult
-		fmt.Fprintln(writer, fmt.Sprintf("%s\t%v", v.TestName, averageDuration))
-	}
-	//noinspection GoUnhandledErrorResult
-	writer.Flush()
-}
-
-func printTestSuccessRate(aggregateTestInfo []*AggregateTestInfo) {
-	for _, v := range aggregateTestInfo {
-		aggregateTestInfo = append(aggregateTestInfo, v)
-	}
-	sort.Slice(aggregateTestInfo, func(i, j int) bool {
-		return (
-			aggregateTestInfo[i].SuccessCount * aggregateTestInfo[j].FailureCount >
-				aggregateTestInfo[j].SuccessCount * aggregateTestInfo[i].FailureCount)
-	})
-
-	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
-	//noinspection GoUnhandledErrorResult
-	fmt.Fprintln(writer, "Test name\tSuccess Rate")
-	//noinspection GoUnhandledErrorResult
-	fmt.Fprintln(writer, "----------\t-----------")
-	for _, v := range aggregateTestInfo {
-		successRate := int32(0)
-		if v.SuccessCount + v.FailureCount > 0 {
-			successRate = (100 * v.SuccessCount) / (v.SuccessCount + v.FailureCount)
+	if len(*testStatusFilter) > 0 {
+		if result.Status != *testStatusFilter {
+			return false
 		}
-		//noinspection GoUnhandledErrorResult
-		fmt.Fprintln(writer, fmt.Sprintf("%s\t%d/%d (%d%%)",
-			v.TestName, v.SuccessCount, v.SuccessCount + v.FailureCount, successRate))
 	}
-	//noinspection GoUnhandledErrorResult
-	writer.Flush()
+	return true
 }
 
-func getTime(timeString string) time.Time {
-	parsedTime, err := time.Parse(time.RFC3339Nano, timeString)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to parse parsedTime \"%v\", error: %v", timeString, err))
-	}
-	return parsedTime
-}
+//func filterByBranchName(results []CircleCiBuildResult, branchName string) []CircleCiBuildResult {
+//	newResults := make([]CircleCiBuildResult, 0)
+//	for _, result := range results {
+//		if result.Branch == branchName {
+//
+//		}
+//	}
+//}
 
-func getTestDuration(buildResult CircleCiBuildResult) time.Duration {
-	startTime := getTime(buildResult.StartTime)
-	endTime := getTime(buildResult.EndTime)
-	return endTime.Sub(startTime)
+func logDebug(msg string) {
+	if !debug {
+		return
+	}
+	fmt.Println(msg)
 }
